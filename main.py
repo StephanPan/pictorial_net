575b1ea7ee2ba657f5ee3a03cf748bd78601d8cb
StephanPan <37864281+StephanPan@users.noreply.github.com>
2020-07-16 09:38:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ class AverageMeter(object):
 def convert_to_unit_vector(angles):
     x = -torch.cos(angles[:, 0]) * torch.sin(angles[:, 1])
     y = -torch.sin(angles[:, 0])
-    z = -torch.cos(angles[:, 1]) * torch.cos(angles[:, 1])
+    z = -torch.cos(angles[:, 0]) * torch.cos(angles[:, 1])
     norm = torch.sqrt(x**2 + y**2 + z**2)
     x /= norm
     y /= norm
@@ -116,7 +116,7 @@ def convert_to_unit_vector(angles):
 def compute_angle_error(preds, labels):
     pred_x, pred_y, pred_z = convert_to_unit_vector(preds)
     label_x, label_y, label_z = convert_to_unit_vector(labels)
-    angles = pred_x * label_x + pred_y * label_y + pred_z * label_z
+    angles = pred_x * label_x + pred_y * label_y + pred_z * label_z + 1e-7
     return torch.acos(angles) * 180 / np.pi
 
 
